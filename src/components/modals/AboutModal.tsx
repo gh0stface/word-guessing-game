6ab73390b02666a0a19a02891996b2b90b890687
gh0stface <scripts@gh0stface.com>
2022-02-05 14:51:12 -0500

    customize

--- src/components/modals/AboutModal.tsx
+++ src/components/modals/AboutModal.tsx
@@ -9,12 +9,18 @@ export const AboutModal = ({ isOpen, handleClose }: Props) => {
   return (
     <BaseModal title="About" isOpen={isOpen} handleClose={handleClose}>
       <p className="text-sm text-gray-500 dark:text-gray-300">
-        This is an open source word guessing game -{' '}
+        This is a fork of an open source word guessing game
+      </p>
+      <p className="text-sm text-gray-500 dark:text-gray-300">
+        Modified by Abrom Douglas
+      </p>
+      <p className="text-sm text-gray-500 dark:text-gray-300">
+        Check out the source code {' '}
         <a
-          href="https://github.com/hannahcode/GAME"
+          href="https://ad3.in/word-guess"
           className="underline font-bold"
         >
-          check out the code here
+          here
         </a>{' '}
       </p>
     </BaseModal>
