10a95eeb684a034e496f94e5f1ba339aa1bd7812
gh0stface <scripts@gh0stface.com>
2022-02-07 23:07:35 -0500

    updated sharing missed letter color

--- src/lib/share.ts
+++ src/lib/share.ts
@@ -22,7 +22,7 @@ export const generateEmojiGrid = (guesses: string[]) => {
             case 'present':
               return '🟨'
             default:
-              return '⬜'
+              return '🔳'
           }
         })
         .join('')
